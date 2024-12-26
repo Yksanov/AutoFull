5137565de997c2f01f3a632d75bc3b5d8c45501d
e.yksanov <e.yksanov@onoi.me>
2024-12-26 17:25:57 +0600

    added new ViewModels: _IndexPartialView and MarcaViewModel

---
+++ ViewModels/MarcaViewModel.cs
@@ -0,0 +1,9 @@
+using AutoFull.Models;
+
+namespace AutoFull.ViewModels;
+
+public class MarcaViewModel
+{
+    public Marca MarcasDetais { get; set; }
+    public MyUser CurrentUser { get; set; }
+}
